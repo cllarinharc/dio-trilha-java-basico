7f585871f64418245443b3697ec0af0ffdd1c638
Ana Clara <anaclara1905@hotmail.com>
2024-03-24 17:26:28 -0300

    Atividade-smart-tv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+  
+  public static void main(String[] args){
+    String nomeCompleto = "Linguagem " + "Java";
+    System.out.println(nomeCompleto);
+
+  //classe Operadores.java
+    int numero = 5;
+		
+  //Imprimindo o numero negativo
+    System.out.println(- numero);
+
+  //incrementando numero em mais 1 numero, imprime 6
+    numero ++;
+    System.out.println(numero);
+
+  //incrementando numero em mais 1 numero, imprime 7
+    System.out.println(numero ++);// ops algo de errado não está certo
+
+    System.out.println(numero);// agora sim, numero virou 7
+
+  //ordem de precedencia conta aqui
+    System.out.println(++ numero);
+
+    boolean verdadeiro = true;
+
+    System.out.println("Inverteu " + !verdadeiro);
+  }
+}
